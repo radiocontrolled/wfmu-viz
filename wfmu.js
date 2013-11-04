a2f36c727dd0ec0e31539cde4f449d09b9345a47
radiocontrolled <radiocontrolled@gmail.com>
2013-11-04 21:10:53 +0000

    playing with force layout

--- wfmu.js
+++ wfmu.js
@@ -1,67 +1,82 @@
 
 //width and height of the canvas
-var w = 500;
-var h = 200;
+var w =  document.body.clientWidth;
+var h =  w/2;
+var cx = 20;
+var r = 10;
+var row = document.body.clientWidth / (r/2);
 
-//make an SVG element and append it to the body
-var svg = d3.select("body")
-	.append("svg")
-	.attr("width", w)
-	.attr("height", h);
+
+/* http://jsonviewer.stack.hu/#http://freemusicarchive.org/recent.jsonp? */
 
 /*
- * grab jsonp file of recent tracks added to the FMA archive
+ * grab json file of recent tracks added to the FMA archive
  * see http://freemusicarchive.org/api/docs/ for more details
  */
-
 function requestRecentTracks() {
   d3.jsonp("http://freemusicarchive.org/recent.jsonp?callback=foo");
 }
 
+
+var link = {
+			edges: [
+					{source: 0, target: 20},
+					{source: 1, target: 20},
+					{source: 2, target: 20},
+					{source: 3, target: 20},
+					{source: 4, target: 20},
+					{source: 5, target: 20},
+					{source: 6, target: 20},
+					{source: 7, target: 20},
+					{source: 8, target: 20},
+					{source: 9, target: 20},
+					{source: 10, target: 20},
+					{source: 11, target: 20},
+					{source: 12, target: 20},
+					{source: 13, target: 20},
+					{source: 14, target: 20},
+					{source: 15, target: 20},
+					{source: 16, target: 20},
+					{source: 17, target: 20},
+					{source: 18, target: 20},
+					{source: 19, target: 20}					
+				]
+			};
+
 function foo(data){
-	var result = {}; 
-	for(var i = 0; i < data.aTracks.length; i++){
+	
+	 var result = {};
+        for(var i = 0; i < data.aTracks.length; i++){
+         	if(i==19){
+         		data.aTracks.push("Parent");
+         	}
+           
+        }
+
+	//make an SVG element and append it to the article
+	 var svg = d3.select("article")
+		.append("svg")
+		.attr("width", w)
+		.attr("height", h);
+
+	//add circles for every new track	
+   	 svg.selectAll("circle")
+		.data(data.aTracks)
+		.enter()
+		.append("g")	
+		.append("circle").attr("r",r)
+		/*.attr({"r":r,
+		       "cy":200,
+		       "cx": function(d,i){
+		       	return  (i % row) * (r*2);
+		       }
+		      })*/
 		
-		/*
-		 * count the # of times an artist appears
-		 * store it in the result object
-		 * because the value (result[key]) will be used as the radius of the circle
-		 */
-		if(!result[data.aTracks[i].artist_name])
-	        result[data.aTracks[i].artist_name] = 0;
-	    	++result[data.aTracks[i].artist_name];
-	    
-	}
-	
-	/*
-	 * draw circle with radius proportional to 
-	 * the number of tracks by the artist that have been recently added.
-	 */
-	for(key in result){
-		svg.append("circle")
-			//come up with a more sustainable styling option because this isn't going to work when there are tracks by many different artists.
-			.attr({
-				"r":result[key]*2.5,  
-				"cy":result[key]*10,
-				"cx":result[key]*i+10
-			})
-			.style({
-				"stroke": function(d,i){return "rgb(221, 65," + (result[key] * 16) +" )";},
-				"fill":"none",
-				"stroke-width":2
-			})
-			.append("text")
-			.text(key)
-			.style({
-					"fill": "black",
-      				"font-family": "Helvetica",
-      				"font-size": "12px"
-			})
-			
-	}
+		.style("fill","red");
+
+     var force = d3.layout.force()
+     	.nodes(data.aTracks)
 		
-	
-	
 }
 
 requestRecentTracks();
