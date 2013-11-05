f84376fd2b298cda7eff0cee3cf06ca1f0296a39
radiocontrolled <radiocontrolled@gmail.com>
2013-11-05 20:13:15 +0000

    add force diagram layout

--- wfmu.js
+++ wfmu.js
@@ -7,8 +7,6 @@ var r = 10;
 var row = document.body.clientWidth / (r/2);
 
 
-/* http://jsonviewer.stack.hu/#http://freemusicarchive.org/recent.jsonp? */
-
 /*
  * grab json file of recent tracks added to the FMA archive
  * see http://freemusicarchive.org/api/docs/ for more details
@@ -18,30 +16,6 @@ function requestRecentTracks() {
 }
 
 
-var link = {
-			edges: [
-					{source: 0, target: 20},
-					{source: 1, target: 20},
-					{source: 2, target: 20},
-					{source: 3, target: 20},
-					{source: 4, target: 20},
-					{source: 5, target: 20},
-					{source: 6, target: 20},
-					{source: 7, target: 20},
-					{source: 8, target: 20},
-					{source: 9, target: 20},
-					{source: 10, target: 20},
-					{source: 11, target: 20},
-					{source: 12, target: 20},
-					{source: 13, target: 20},
-					{source: 14, target: 20},
-					{source: 15, target: 20},
-					{source: 16, target: 20},
-					{source: 17, target: 20},
-					{source: 18, target: 20},
-					{source: 19, target: 20}					
-				]
-			};
 
 function foo(data){
 	
@@ -52,6 +26,32 @@ function foo(data){
          	}
            
         }
+        
+     var link = {
+			edges: [
+					{source: 0, target: 19},
+					{source: 1, target: 19},
+					{source: 2, target: 19},
+					{source: 3, target: 19},
+					{source: 4, target: 19},
+					{source: 5, target: 19},
+					{source: 6, target: 19},
+					{source: 7, target: 19},
+					{source: 8, target: 19},
+					{source: 9, target: 19},
+					{source: 10, target: 19},
+					{source: 11, target: 19},
+					{source: 12, target: 19},
+					{source: 13, target: 19},
+					{source: 14, target: 19},
+					{source: 15, target: 19},
+					{source: 16, target: 19},
+					{source: 17, target: 19},
+					{source: 18, target: 19},
+					{source: 19, target: 19}					
+				]
+			};
+
 
 	//make an SVG element and append it to the article
 	 var svg = d3.select("article")
@@ -59,24 +59,52 @@ function foo(data){
 		.attr("width", w)
 		.attr("height", h);
 
+	
 	//add circles for every new track	
-   	 svg.selectAll("circle")
+   	var nodes = svg.selectAll("circle")
 		.data(data.aTracks)
 		.enter()
-		.append("g")	
 		.append("circle").attr("r",r)
-		/*.attr({"r":r,
-		       "cy":200,
-		       "cx": function(d,i){
-		       	return  (i % row) * (r*2);
-		       }
-		      })*/
+		.style("fill","red")
 		
-		.style("fill","red");
-
-     var force = d3.layout.force()
+		
+	
+	
+		
+	// Create an SVG line for each edge
+	var edges = svg.selectAll("line")
+		 .data(link.edges)
+		 .enter()
+		 .append("line")
+		 .style("stroke", "#ccc")
+		 .style("stroke-width", 1);
+		
+		        
+	 //initialize a force layout
+	 var force = d3.layout.force()
      	.nodes(data.aTracks)
+     	.links(link.edges)
+     	.size([w, h])
+        .linkDistance([200]) //distance between the nodes 
+        .charge([-150])     // repelling between the nodes       
+    	.start();
+    	
+    
+    
+    force.on("tick", function() {
+    	
+    	edges.attr("x1", function(d) { return d.source.x; })
+	      .attr("y1", function(d) { return d.source.y; })
+	      .attr("x2", function(d) { return d.target.x; })
+	      .attr("y2", function(d) { return d.target.y; });
+
+	  	nodes.attr("cx", function(d) { return d.x; })
+	      .attr("cy", function(d) { return d.y; })
+	      .call(force.drag); //let the nodes be draggable.
 		
+		});
+ 
+  		
 }
 
 requestRecentTracks();
